9a92479b6d4362ded7f08401c691debfed7265e5
Koushik Ahmed <koushikxahmed@gmail.com>
2023-08-01 22:39:11 +0600

    display qoutes and next, prev done

--- src/Review.jsx
+++ src/Review.jsx
@@ -6,7 +6,36 @@ const Review = () => {
   const [index, setIndex] = useState(0);
   const { id, name, job, image, text } = people[index];
   console.log(people);
-  return <h2>review component</h2>;
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button
+          className="prev-btn"
+          disabled={index === 0 ? true : false}
+          onClick={() => setIndex(index - 1)}
+        >
+          <FaChevronLeft />
+        </button>
+        <button
+          className="next-btn"
+          disabled={index === people.length - 1 ? true : false}
+          onClick={() => setIndex(index + 1)}
+        >
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className="random-btn">Random Quote</button>
+    </article>
+  );
 };
 
 export default Review;
