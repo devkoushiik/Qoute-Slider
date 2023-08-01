bc973280da57359789e2f347967e5f1a30f6119d
Koushik Ahmed <koushikxahmed@gmail.com>
2023-08-01 22:53:08 +0600

    disable problem solve, random button added

--- src/Review.jsx
+++ src/Review.jsx
@@ -5,7 +5,21 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 const Review = () => {
   const [index, setIndex] = useState(0);
   const { id, name, job, image, text } = people[index];
-  console.log(people);
+
+  const checkPerson = (number) => {
+    if (number > people.length - 1) return 0;
+    if (number < 0) return people.length - 1;
+    return number;
+  };
+
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.floor(Math.random() * people.length));
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    setIndex(checkPerson(randomNumber));
+  };
+
   return (
     <article className="review">
       <div className="img-container">
@@ -20,20 +34,20 @@ const Review = () => {
       <div className="button-container">
         <button
           className="prev-btn"
-          disabled={index === 0 ? true : false}
-          onClick={() => setIndex(index - 1)}
+          onClick={() => setIndex(checkPerson(index - 1))}
         >
           <FaChevronLeft />
         </button>
         <button
           className="next-btn"
-          disabled={index === people.length - 1 ? true : false}
-          onClick={() => setIndex(index + 1)}
+          onClick={() => setIndex(checkPerson(index + 1))}
         >
           <FaChevronRight />
         </button>
       </div>
-      <button className="random-btn">Random Quote</button>
+      <button onClick={randomPerson} className="random-btn">
+        Random Quote
+      </button>
     </article>
   );
 };
