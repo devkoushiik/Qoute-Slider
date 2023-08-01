27310137c1b4eb05f8cb9cf917453ef56b7dcb98
Koushik Ahmed <koushikxahmed@gmail.com>
2023-08-01 22:21:43 +0600

    basic pipeline setup done

---
+++ src/Review.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+
+const Review = () => {
+  const [index, setIndex] = useState(0);
+  const { id, name, job, image, text } = people[index];
+  console.log(people);
+  return <h2>review component</h2>;
+};
+
+export default Review;
+
+//
